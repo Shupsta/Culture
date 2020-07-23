072bb779f1399aa2b0bad9af59a217f5e22d737f
Nick S <shupsta2010@gmail.com>
2020-07-22 18:27:58 -0700

    initial loading bar commit

--- src/scenes/Bunkakai.js
+++ src/scenes/Bunkakai.js
@@ -4,8 +4,69 @@ class Bunkakai extends Phaser.Scene{
     }
 
     preload(){
+
+        //Config object for Loading text
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '60px',
+            backgroundColor: '#FACADE',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+
+        }
+
+        this.add.text(centerX, centerY - 64, 'Loading', menuConfig).setOrigin(0.5);//writes loading text above loading bar
+
+        /**
+         * Progress bar credit to 
+         * https://gamedevacademy.org/creating-a-preloading-screen-in-phaser-3/
+         */
+        var progressBar = this.add.graphics();//creates progressBar
+        var progressBox = this.add.graphics();//boarder around progressBar
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(centerX - 320 / 2, centerY, 320, 50);
+
+        this.load.on('progress', function (value) {//uses the built in progress event from Phaser
+            console.log(value);
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            progressBar.fillRect(centerX - 320 / 2, centerY, 300 * value, 30);
+        });
+                    
+        this.load.on('fileprogress', function (file) {//can be used to display info of each loading file
+            console.log(file.src);
+        });
+         
+        this.load.on('complete', function () {//phaser event for end of loading to destroy the loading screen
+            console.log('complete');
+            progressBar.destroy();
+            progressBox.destroy();
+        });
+
+
+
+
         // load splash screen
         this.load.image('splash_screen', './assets/splash.jpg');
+
+        //loading from Art Scene
+        this.load.image('redHeart', './assets/redHeart.png');
+        this.load.image('sidewalk', './assets/sidewalk.png');
+        this.load.image('hills', './assets/hills.png');
+        this.load.image('sky', './assets/sky.png');
+        this.load.image('nightSky', './assets/starryBackground.jpg');
+        this.load.image('circle', './assets/circle-8x8.png');
+        this.load.image('moon', './assets/moon.png');
+        this.load.image('staryNight', './assets/staryNight.png');
+        this.load.image('fields', './assets/fields.png');
+        this.load.image('bridge', './assets/bridge.png');
+
+
         // load audio files
         this.load.audio('sfx_select', './assets/iPhoneCameraSound.mp3');
         this.load.audio('sfx_explosion', './assets/sagoi.wav');
@@ -15,6 +76,8 @@ class Bunkakai extends Phaser.Scene{
         this.load.audio('fashionbgm', './assets/bunkakaiFashionGroove.mp3');
         this.load.audio('musicbgm', './assets/bunkakaiFashionGroove.mp3');
         this.load.audio('ohno', './assets/ohno.wav');
+
+
     }
 
 
